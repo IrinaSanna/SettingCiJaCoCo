0c13f6690b33f2e698d1fe7b966ac48bd6dc237e
Irina D <RabotaYd2021@yandex.ru>
2024-02-20 17:15:45 +0300

    Added code coverage test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,7 +10,20 @@ public class StatisticsServiceTest {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+
         long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void finMaxInEnd() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 23, 12};
+
+        long expected = 23;
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
